9d8b385b9da2b1cc6f58b1fe3d8795f9fc32785e
Rex Wei <wytings@163.com>
2018-07-19 01:02:32 +0800

    add infinite list

--- lib/main.dart
+++ lib/main.dart
@@ -1,167 +1,54 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp(
+      home: RandomWords(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class RandomWords extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return RandomState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int index = 0;
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+class RandomState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildList(),
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(itemBuilder: (context, i) {
+      if (i.isOdd) {
+        return Divider();
+      }
+      final index = i ~/ 2;
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(index);
     });
   }
 
-  void _showSnackbar(String msg) {
-    _scaffoldKey.currentState.showSnackBar(
-        new SnackBar(content: new Text(msg)));
-  }
-
-  void _showDialog() {
-    showDialog(context: context,
-        barrierDismissible: false,
-        child: new CupertinoAlertDialog(title: new Text('title'),
-          content: new Text('content'),
-          actions: <Widget>[new CupertinoDialogAction(onPressed: () {
-            Navigator.pop(context, 1);
-          },
-              child: new Text(
-                  'Confirm', textAlign: TextAlign.center, style: Theme
-                  .of(context)
-                  .textTheme
-                  .title))
-          ],));
-  }
-
-  void _showAndroidDialog() {
-    showDialog(context: context,
-        barrierDismissible: false,
-        child: new AlertDialog(
-          title: new Text("My Super title"),
-          content: new Text("Hello World"),
-        )
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      key: _scaffoldKey,
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      bottomNavigationBar: new BottomNavigationBar(items: [
-        new BottomNavigationBarItem(
-            icon: new Icon(Icons.add),
-            title: new Text("first"))
-        ,
-        new BottomNavigationBarItem(
-            icon: new Icon(Icons.dashboard),
-            title: new Text("second"))
-        ,
-        new BottomNavigationBarItem(
-            icon: new Icon(Icons.satellite),
-            title: new Text("second"))
-      ],
-          currentIndex: index,
-          onTap: (i) {
-            setState(() {
-              this.index = i;
-            });
-          }),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: () {
-          _showDialog();
-        },
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  Widget _buildRow(int index) {
+    return ListTile(
+      title: Text("$index-${_suggestions[index].asPascalCase}"),
     );
   }
 }
